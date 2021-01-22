25dff5f5de5c138b41d5f0f617ef621550564ae4
Wenyu <wenyu_zhang@brown.edu>
2021-01-22 16:59:41 -0500

    condor script batch new setup

--- step2/makeStep2condor.py
+++ step2/makeStep2condor.py
@@ -9,9 +9,9 @@ shift = sys.argv[1]
 foldnum = '-1'
 relbase   = '/home/wzhang/work/fwljmet_201905/CMSSW_10_2_16_UL/'
 # inputDir  = '/mnt/hadoop/users/ssagir/LJMet94X_1lepTT_020619_step1hadds/'+shift+'/'
-inputDir  = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2018_Oct2019_4t_031920_step1hadds/'+shift+'/'
+inputDir  = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2016_Feb2020_4t_080420_step1hadds/'+shift+'/'
 # outputDir = '/mnt/hadoop/users/jlee/TTTT/LJMet94X_1lepTT_022219_step2/'+shift+'/'
-outputDir = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2018_Oct2019_4t_03302020_step2/'+shift+'/'
+outputDir = '/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2016_Feb2020_4t_01212021_step2/'+shift+'/'
 runDir=os.getcwd()
 gROOT.ProcessLine('.x compileStep2.C')
 
@@ -46,13 +46,13 @@ Transfer_Input_Files = %(RUNDIR)s/makeStep2.C, %(RUNDIR)s/step2.cc, %(RUNDIR)s/s
 Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
 Log = %(FILENAME)s.log
+JobBatchName = <batch_name>
 Notification = Never
 Arguments = %(FILENAME)s.root %(FILENAME)s.root %(INPUTDIR)s %(OUTPUTDIR)s
-
 Queue 1"""%dict)
     jdf.close()
     os.chdir('%s/'%(condorDir))
-    os.system('condor_submit %(FILENAME)s.job'%dict)
+    os.system('condor_submit %(FILENAME)s.job -batch-name step2_wzhang'%dict)
     os.system('sleep 0.5')                                
     os.chdir('%s'%(runDir))
     print count, "jobs submitted!!!"
